020b366b17c76f2af2cc112fc2b453b9d177fb61
svetlana <svetlana.vl.koroleva@gmail.com>
2023-06-15 16:34:25 +0400

    Add examples

--- tests/test_api.py
+++ tests/test_api.py
@@ -17,21 +17,6 @@ def test_meow_api(lang, id_):
     assert r.json()['data']
 
 
-@pytest.mark.parametrize("lang, id",
-                         [('rus', 12),
-                          ('urd', 1),
-                          ('esp', 31),
-                          ('ita', 13),
-                          ('zho', 22)])
-def test_meow_api(lang, id):
-    query = {'lang': lang,
-             'id': id}
-
-    r = requests.get('https://meowfacts.herokuapp.com/', params=query)
-    assert r.status_code == 200
-    assert r.json()['data']
-
-
 @pytest.mark.parametrize('lang', ['rus', 'urd', 'esp', 'ita', 'zho'])
 @pytest.mark.parametrize('id_', [12, 1, 31, 13, 22])
 def test_meow_api_combo(lang, id_):
